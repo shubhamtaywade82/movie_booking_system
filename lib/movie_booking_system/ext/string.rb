9db20c36b6f8b5e7ad7e3c9d06553418e724992c
Shubham Taywade <shubhamtaywade82@gmail.com>
2024-07-16 20:52:17 +0530

    Extend String class with singularize method

--- lib/movie_booking_system/ext/string.rb
+++ lib/movie_booking_system/ext/string.rb
@@ -12,4 +12,16 @@ class String
       "#{self}s"
     end
   end
+
+  def singularize
+    if end_with?("ies") && !end_with?("aies", "eies", "iies", "oies", "uies")
+      "#{self[0..-4]}y"
+    elsif end_with?("zes")
+      (self[0..-4]).to_s
+    elsif end_with?("es") && end_with?("ses", "xes", "ches", "shes")
+      (self[0..-3]).to_s
+    else
+      end_with?("s") ? (self[0..-2]).to_s : self
+    end
+  end
 end
